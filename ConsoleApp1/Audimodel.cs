9f5952d9d1e91fd49156e6ccf6b6441a48e2e96f
Ahmedphd1 <tib-tob@hotmail.com>
2021-03-10 04:38:29 +0100

    opgave3

---
+++ ConsoleApp1/Audimodel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Audimodel
+    {
+        public double motorstørelse { get; set; }
+        public DateTime årgang { get; set; }
+        public string model { get; set; }
+        public string farve { get; set; }
+
+        internal bool Any(Func<object, bool> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace ConsoleApp1
 {
     class Program
     {
+
+        delegate Program createlist();
+
+        static Program tilføjliste()
+        {
+            Program myprogram = new Program();
+
+            //Audi instances
+            Audimodel model1 = new Audimodel();
+            DateTime dt = new DateTime(2000, 03, 03);
+            model1.årgang = dt;
+            model1.farve = "blå";
+
+            Audimodel model2 = new Audimodel();
+            DateTime dt2 = new DateTime(1994, 03, 03);
+            model2.årgang = dt2;
+            model2.farve = "rød";
+
+            Audimodel model3 = new Audimodel();
+            DateTime dt3 = new DateTime(2928, 03, 03);
+            model3.årgang = dt3;
+            model3.farve = "gul";
+
+            Audimodel model4 = new Audimodel();
+            DateTime dt4 = new DateTime(5948, 03, 03);
+            model4.årgang = dt4;
+            model4.farve = "sort";
+
+            Audimodel model5 = new Audimodel();
+            DateTime dt5 = new DateTime(7676, 03, 03);
+            model5.årgang = dt5;
+            model5.farve = "sort";
+
+            myprogram.mylist.Add(model1);
+            myprogram.mylist.Add(model2);
+            myprogram.mylist.Add(model3);
+            myprogram.mylist.Add(model4);
+            myprogram.mylist.Add(model5);
+
+            return myprogram;
+        }
+
+        public List<Audimodel> mylist { get; set; } = new List<Audimodel>();
         static void Main(string[] args)
         {
-            Audi nyaudi = new Audi("bmw");
+            createlist thislist = tilføjliste;
 
-            nyaudi.printproperties();
-            nyaudi.printproperties("bilen er skadet");
+            Program audiliste = thislist();
+
+            try
+            {
+                var query = audiliste.mylist.Where(element => element.farve == "sort").Select(element => element);
+
+                foreach (var element in query)
+                {
+                    Console.WriteLine(element.model);
+                    Console.WriteLine(element.årgang);
+                    Console.WriteLine(element.farve);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("liste er tomt");
+            }
+            }
         }
     }
-}
